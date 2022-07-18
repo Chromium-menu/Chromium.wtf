deed268d39ea87cb5d39c77529b2e3a8fc8d644e
OkPeach <lucienismeme@gmail.com>
2022-07-18 16:39:55 +0200

    Shadows and animations WIP

--- js/script.js
+++ js/script.js
@@ -31,3 +31,18 @@ function navCheck(entries) {
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+const inViewport = (entries, observer) => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+  });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+// Attach observer to every [data-inviewport] element:
+const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+ELs_inViewport.forEach(EL => {
+  Obs.observe(EL, obsOptions);
+});
