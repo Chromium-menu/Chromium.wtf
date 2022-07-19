2789b5d21338611e313751cbb2dc5736bacca035
OkPeach <lucienismeme@gmail.com>
2022-07-20 00:09:43 +0200

    Added loading animation

--- js/script.js
+++ js/script.js
@@ -46,3 +46,13 @@ const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
 ELs_inViewport.forEach(EL => {
   Obs.observe(EL, obsOptions);
 });
+
+$(window).on('load', function () {
+  setTimeout(function () {
+    //locate "target" aka the preload screen and slowly transition to 0 opacity and after transition deletes the whole element
+    const target = document.getElementById("loading");
+    target.style.opacity = '0'
+    target.addEventListener('transitionend', () => target.remove());
+  }, 1250);
+  console.log('Loaded, welcome!')
+}) 
