6e56883616334a6f08b4eb7db0ec6c75553d00eb
OkPeach <lucienismeme@gmail.com>
2022-07-20 13:27:44 +0200

    New animations and loading animation temp off

--- js/script.js
+++ js/script.js
@@ -50,9 +50,9 @@ ELs_inViewport.forEach(EL => {
 $(window).on('load', function () {
   setTimeout(function () {
     //locate "target" aka the preload screen and slowly transition to 0 opacity and after transition deletes the whole element
-    const target = document.getElementById("loading");
-    target.style.opacity = '0'
-    target.addEventListener('transitionend', () => target.remove());
+    //const target = document.getElementById("loading");
+    //target.style.opacity = '0'
+    //target.addEventListener('transitionend', () => target.remove());
   }, 1250);
   console.log('Loaded, welcome!')
 }) 
